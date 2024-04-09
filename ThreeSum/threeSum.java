6852d8e7bc4e48decafde2edd1dfcbd92081e22f
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-04-09 22:28:49 +0530

    practice

---
+++ ThreeSum/threeSum.java
@@ -0,0 +1,31 @@
+package ThreeSum;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+public class threeSum {
+      private static List<List<Integer>> threeSumBrute(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        HashSet<List<Integer>> res=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    if((nums[i]+nums[j]+nums[k])==0)
+                    {
+                      System.out.println("["+nums[i]+","+nums[j]+","+nums[k]+"]");
+                        res.add(List.of(nums[i],nums[j],nums[k]));
+                        
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+    public static void main(String[] args) {    
+      threeSumBrute(new int[]{-1,1,2,4,-2,-2,3,-3,-4}); 
+
+    }
+}
