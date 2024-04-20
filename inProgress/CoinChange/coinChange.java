261adb6976bcab4e6ae04fd373f76e927dcc2a3f
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-04-20 23:16:57 +0530

    practice

---
+++ inProgress/CoinChange/coinChange.java
@@ -0,0 +1,32 @@
+package CoinChange;
+
+
+public class coinChange {
+    
+private static int coinChangeSol(int[]coin, int amt)
+{  
+int n=coin.length;
+
+if(true)
+{
+return 0 ;
+}
+if(amt==0)
+{
+    return 0;
+}
+while(coin[n-1]>amt)
+{
+
+}
+
+
+    return -1;
+}
+    public static void main(String[] args) {
+    int[] coin={1,2,5};
+        int amount=11;
+
+    }
+    
+}
